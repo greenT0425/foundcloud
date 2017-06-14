22292618c08df3dd1db9799813eb5fa90375b7c0
greenT <tetsuro.recruit@gmail.com>
2017-06-14 16:08:33 +0000

    add splash View

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	root to: 'splash#index'
+	
+	# トラック一覧
+	get 'tracks', to: 'tracks#index'
+	
+	# TODO アーティスト・アーティスト検索
+	# resources :artists,only: [:index] do
+	# 	collection do
+	# 		get :search
+	# 	end
+	# end
+	
+	# TODO お気に入り機能やるなら
+	# resources :favorites, only: [:create, :destroy]
 end
